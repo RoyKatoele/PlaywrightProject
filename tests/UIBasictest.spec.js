dff3629c4c447447080cd98c5d56a11851c2ea37
RoyKatoele <roykatoele@gmail.com>
2025-07-26 22:43:24 +0200

    verdere uitbreiding van de test

--- tests/UIBasictest.spec.js
+++ tests/UIBasictest.spec.js
@@ -11,14 +11,21 @@ test.only('First Playwright test',async function({browser}) // andere manier van
 //chrome - plugins/ cookies
 const context = await browser.newContext();
 const page = await context.newPage();
+const userName = page.locator('#username'); // je kunt een locator ook in een variabele opslaan zoals hier gedaan is
+const signIn = page.locator('#signInBtn')
 await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
 console.log(await page.title());
-await page.locator('#username').fill("rahulshetty"); // hier gaat hij op zoek naar het element met de ID (username) en vult deze met de waarde in fill
+await userName.fill("rahulshetty"); // hier gaat hij op zoek naar het element met de ID (username) en vult deze met de waarde in fill
 await page.locator("[type='password']").fill("learning"); // hier gaat hij op zoek naar het element "type='password'"en vult deze met de waarde in fill
-await page.locator('#signInBtn').click(); // de knop met het ID "signInBtn" wordt geklikt
+await signIn.click(); // de knop met het ID "signInBtn" wordt geklikt
 // deze wait hieronder zorgt ervoor dat er gewacht wordt met de inlog foutmelding checken tot deze ook echt in beeld is. 'textContent' geeft de waarde in het foutmeldingsveld weer  
 console.log (await page.locator("[style*='block']").textContent()); // het * na de 'style' zorgt er voor dat je op een stukje van een waarde kan zoeken in dit geval wat de gehele value "display: block;"
 await expect(page.locator("[style*='block']")).toContainText('Incorrect'); //deze stap haalt de waarde op in het element 'style*='block'' en controleert dat deze deels het woord 'incorrect' bevat
+await userName.fill("");
+await userName.fill("rahulshettyacademy");
+await signIn.click();
+console.log (await page.locator(".card-body a").nth(0).textContent()); // deze locator vindt 4 elementen. om te zorgen dat hij de eerste terug geeft kun je ".nth(0)" toevoegen als 1 invult zal hij het tweede element ophalen
+console.log (await page.locator(".card-body a").first().textContent()); // dit is een andere optie
 }
 );
 
